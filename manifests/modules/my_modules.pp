5c827b01a7e991415f18669d08caf13168e374da
bartjanssens92 <bartjanssens92@gmail.com>
2014-02-20 14:44:44 +0100

    first commit

---
+++ manifests/modules/my_modules.pp
@@ -0,0 +1,50 @@
+# Bart Janssens 19-02-14
+
+# Installing the modules
+
+# installing apache
+
+#package { "httpd": ensure	=> present; }
+
+#class { 'apache': }
+
+# logstash
+
+class {'logstash':
+
+	ensure			=> 'present',
+	java_install 	=> true,
+	status			=> 'running',
+
+}
+
+# redis
+
+package {"redis": 
+
+	ensure		=> 'present',
+	status		=> 'running',
+
+}
+
+# elasticsearch clean
+
+class { 'elasticsearch': 
+
+	status		=> 'running',
+
+} 
+
+# kibana clean
+
+package { "ruby": ensure	=> present; }
+
+package { "rubygems": ensure	=> present; }
+
+#class { 'Kibana': }
+
+# Start all the things
+
+service { "redis": enable 	=> true; }
+
+	#status		=> 'running',
---
+++ manifests/site.pp
@@ -0,0 +1,7 @@
+# Bart Janssens 20-02-14
+
+#package {"ruby":  ensure => present; } #moved to the modules.pp
+
+import 'repo/*.pp'
+
+import 'modules/*.pp'
